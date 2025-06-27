3ab8cb23e59d8c55db85c2b39714a1b06a360dcc
GitHub Action <action@github.com>
2025-06-27 11:14:20 +0000

    Initialize template repository proper
    Set package name to com.github.meto1558.clickassist
    Set project name to ClickAssist
    Set modid to clickassist

--- src/main/java/com/github/meto1558/clickassist/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/meto1558/clickassist/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.meto1558.clickassist.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/meto1558/clickassist/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/meto1558/clickassist/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.meto1558.clickassist.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
